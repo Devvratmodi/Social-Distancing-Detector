483585570a90acbce87cea7b33baed6c866d64d0
Devvratmodi <46401872+Devvratmodi@users.noreply.github.com>
2021-08-21 22:14:12 +0530

    Add files via upload

---
+++ mylib/config.py
@@ -0,0 +1,29 @@
+# base path to YOLO directory
+MODEL_PATH = "yolo"
+# initialize minimum probability to filter weak detections along with
+# the threshold when applying non-maxima suppression
+MIN_CONF = 0.3
+NMS_THRESH = 0.3
+
+#===============================================================================
+#=================================\CONFIG./=====================================
+""" Below are your desired config. options to set for real-time inference """
+# To count the total number of people (True/False).
+People_Counter = True
+# Threading ON/OFF.
+Thread = True
+# Set the threshold value for total violations limit.
+Threshold = 15
+# Set url = 0 for webcam.
+url = ''
+# Turn ON/OFF the email alert feature.
+ALERT = False
+# Set mail to receive the real-time alerts. E.g., 'xxx@gmail.com'.
+MAIL = ''
+# Set if GPU should be used for computations; Otherwise uses the CPU by default.
+USE_GPU = False
+# Define the max/min safe distance limits (in pixels) between 2 people.
+MAX_DISTANCE = 80
+MIN_DISTANCE = 50
+#===============================================================================
+#===============================================================================
